1a754022922e440d2c25074ce015e29c2317e62d
Ishan <ishan.kulkarni@somaiya.edu>
2021-10-28 23:58:22 +0530

    Mongo DB created and connected with the book service

--- books/books.js
+++ books/books.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 
 const port = process.env.PORT||5000;
 
+mongoose.connect("mongodb+srv://ishanak1602:ishanak1602@bookservice.wuei5.mongodb.net/test",()=>{
+    console.log("Our Bookservice database is connected!");
+});
 
 app.get('/', (req, res) => {
     res.send('This is our main endpoint');
